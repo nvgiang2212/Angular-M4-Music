48e644275804c4b79f4b3fe596940e8666ddec54
vgiang <Giang221295>
2019-09-05 11:36:40 +0700

    2nd change display header and carousel

--- src/app/component/layout/carousel/carousel.component.ts
+++ src/app/component/layout/carousel/carousel.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class CarouselComponent implements OnInit {
 
   images = [1].map(() => `http://i3.ytimg.com/vi/fAhc2pBo9NA/maxresdefault.jpg`);
-  images1 = [2].map(() => `http://i3.ytimg.com/vi/JlhEbNdog5w/maxresdefault.jpg`);
+  images1 = [2].map(() => `http://i3.ytimg.com/vi/7i63YUbAGcM/maxresdefault.jpg`);
   images2 = [3].map(() => `http://i3.ytimg.com/vi/vm0E1HUm--4/maxresdefault.jpg`);
   images3 = [4].map(() => `http://i3.ytimg.com/vi/Qs-XcmaxaLw/maxresdefault.jpg`);
-  images4 = [5].map(() => `http://i3.ytimg.com/vi/7i63YUbAGcM/maxresdefault.jpg`);
+  images4 = [5].map(() => `http://i3.ytimg.com/vi/JlhEbNdog5w/maxresdefault.jpg`);
 
 
   constructor() { }
