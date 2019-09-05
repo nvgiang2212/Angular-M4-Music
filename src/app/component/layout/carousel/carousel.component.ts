45f56c8542342fe58cb1ff4289142352acda8549
vgiang <Giang221295>
2019-09-05 11:21:21 +0700

    change slide carousel

--- src/app/component/layout/carousel/carousel.component.ts
+++ src/app/component/layout/carousel/carousel.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
 
-  images = [1].map(() => `https://i.ytimg.com/vi/z8S7SZ4sJF0/maxresdefault.jpg`);
-  images1 = [2].map(() => `https://i.ytimg.com/vi/MIGCCcQkV-w/maxresdefault.jpg`);
-  images2 = [3].map(() => `https://i.ytimg.com/vi/jFzfkOulKEo/maxresdefault.jpg`);
-  images3 = [4].map(() => `https://reviewsmoi.com/wp-content/uploads/2019/03/mot-buoc-yeu-van-dam-dau-1.jpg`);
-  images4 = [5].map(() => `https://i.ytimg.com/vi/pz_23dIZgVQ/maxresdefault.jpg`);
+  images = [1].map(() => `http://i3.ytimg.com/vi/fAhc2pBo9NA/maxresdefault.jpg`);
+  images1 = [2].map(() => `http://i3.ytimg.com/vi/JlhEbNdog5w/maxresdefault.jpg`);
+  images2 = [3].map(() => `http://i3.ytimg.com/vi/vm0E1HUm--4/maxresdefault.jpg`);
+  images3 = [4].map(() => `http://i3.ytimg.com/vi/Qs-XcmaxaLw/maxresdefault.jpg`);
+  images4 = [5].map(() => `http://i3.ytimg.com/vi/7i63YUbAGcM/maxresdefault.jpg`);
 
 
   constructor() { }
