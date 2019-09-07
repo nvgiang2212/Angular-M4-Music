1f9a5c42f1eff7b519b79d23ecc97024db4e49d0
nguyenthetinh <tinhnt0207@gmail.com>
2019-09-07 13:33:19 +0700

    test

--- src/app/component/songManager/detail-song/detail-song.component.ts
+++ src/app/component/songManager/detail-song/detail-song.component.ts
@@ -1,26 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import {Song} from "../../../model/song/song";
 import {SongService} from "../../../service/song/song.service";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Route} from '@angular/router';
 
 @Component({
   selector: 'app-detail-song',
   templateUrl: './detail-song.component.html',
   styleUrls: ['./detail-song.component.scss']
 })
+
 export class DetailSongComponent implements OnInit {
 
   song: Song = {
-    id: 0,
-    nameSong: '',
-    nameSinger: '',
-    avatarUrl: '',
-    mp3Url: '',
-    describes: ''
-  };
+      id: 0,
+      nameSong: '',
+      nameSinger: '',
+      avatarUrl: '',
+      mp3Url: '',
+      describes: ''
+    };
 
   constructor(
     private songService: SongService,
+    private route: Route,
     private router: ActivatedRoute) {
   }
 
