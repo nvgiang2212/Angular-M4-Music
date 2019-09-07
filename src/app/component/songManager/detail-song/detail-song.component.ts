3225bc3483bd72a7a34ea607a3efa3cc73bd0401
nguyenthetinh <tinhnt0207@gmail.com>
2019-09-07 13:26:22 +0700

    sua detail

--- src/app/component/songManager/detail-song/detail-song.component.ts
+++ src/app/component/songManager/detail-song/detail-song.component.ts
@@ -1,50 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import {Song} from "../../../model/song/song";
 import {SongService} from "../../../service/song/song.service";
-import {ActivatedRoute, Route} from "@angular/router";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-detail-song',
   templateUrl: './detail-song.component.html',
   styleUrls: ['./detail-song.component.scss']
 })
-
 export class DetailSongComponent implements OnInit {
 
-  ngOnInit(): void {
+  song: Song = {
+    id: 0,
+    nameSong: '',
+    nameSinger: '',
+    avatarUrl: '',
+    mp3Url: '',
+    describes: ''
+  };
+
+  constructor(
+    private songService: SongService,
+    private router: ActivatedRoute) {
   }
 
-
-  constructor() {
+  ngOnInit() {
+    const id = +this.router.snapshot.paramMap.get('id');
+    this.songService.getSongById(id)
+      .subscribe(
+        next => {
+          this.song = next.data;
+          console.log(next.data);
+        },
+        error => {
+          console.log(error);
+          this.song = null;
+        });
   }
-
-  // song: Song = {
-  //     id: 0,
-  //     nameSong: '',
-  //     nameSinger: '',
-  //     avatar: '',
-  //     url: '',
-  //     describes: ''
-  //   };
-  //
-  // constructor(
-  //   private songService: SongService,
-  //   private route: Route,
-  //   private router: ActivatedRoute) {
-  // }
-  //
-  // ngOnInit() {
-  //   const id = +this.router.snapshot.paramMap.get('id');
-  //   this.songService.getSongById(id)
-  //     .subscribe(
-  //       next => {
-  //         this.song = next.data;
-  //         console.log(next.data);
-  //       },
-  //       error => {
-  //         console.log(error);
-  //         this.song = null;
-  //       });
-  // }
 
 }
