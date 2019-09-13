7cca83b3ea6de8de5169dec0a0605ad9e089cdc3
vgiang <Giang221295>
2019-09-13 17:21:04 +0700

    thay doi giao dien 4 chut

--- src/app/component/playlistManager/create-playlist/create-playlist.component.ts
+++ src/app/component/playlistManager/create-playlist/create-playlist.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../../service/userManager/auth/auth.service';
-import {FormControl, FormGroup} from "@angular/forms";
+
 import {PlaylistInfor} from '../../../model/playlist/playlist-Infor';
 import {PlaylistService} from '../../../service/playlistManager/playlist.service';
-import {Router} from "@angular/router";
+import {FormControl, FormGroup} from '@angular/forms';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-create-playlist',
@@ -15,21 +17,20 @@ export class CreatePlaylistComponent implements OnInit {
 
   playlistForm: FormGroup;
   playlist: Partial<PlaylistInfor>;
+
   constructor(private auth: AuthService,
               private router: Router,
               private playlistService: PlaylistService) {
     this.playlistForm = new FormGroup({
-      playlistName : new FormControl('')
-      });
+      playlistName: new FormControl('')
+    });
     this.playlist = {
       playlistName: ''
     };
   }
-  constructor(private auth: AuthService) {
-  }
-
   ngOnInit() {
   }
+
   createPlaylist() {
     console.log(this.playlist);
     this.playlistService.createPlaylist(this.playlist).subscribe(() => {
