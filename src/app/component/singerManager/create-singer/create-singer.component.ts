88d7028bd940ee9291ecaba53cf519d1e16a2d90
vgiang <Giang221295>
2019-09-18 00:02:27 +0700

    add function create-singer

--- src/app/component/singerManager/create-singer/create-singer.component.ts
+++ src/app/component/singerManager/create-singer/create-singer.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {Router} from '@angular/router';
+import {SongService} from '../../../service/song/song.service';
+import {SingerInfo} from '../../../model/singer/singer-info';
+import {SingerManagerService} from '../../../service/singerManager/singer-manager.service';
 
 @Component({
   selector: 'app-create-singer',
@@ -7,9 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateSingerComponent implements OnInit {
   title = 'Thêm Ca Sĩ';
-  constructor() { }
+
+  singerForm: FormGroup;
+  singer: Partial<SingerInfo>;
+
+  constructor(
+    private router: Router,
+    private singerManagerService: SingerManagerService) {
+    this.singerForm = new FormGroup({
+      singerName: new FormControl(''),
+      singerAvatar: new FormControl(''),
+      singerBirthday: new FormControl(''),
+      singerNation: new FormControl(''),
+      description: new FormControl(''),
+    });
+    this.singer = {
+      singerName: '',
+      singerAvatar: '',
+      singerBirthday: '',
+      singerNation: '',
+      description: '',
+    };
+  }
 
   ngOnInit() {
+  }
+
+  onAvatar($event) {
+    this.singer.singerAvatar = $event;
+  }
+
+  createSinger() {
+    console.log(this.singer);
+    this.singerManagerService.createSinger(this.singer).subscribe(() => {
+      this.router.navigate(['/home']);
+    }, error => console.log(error));
   }
 
 }
