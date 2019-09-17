69c8af1ecb247d9462f2af7bc8d14ee89b944373
vgiang <Giang221295>
2019-09-17 11:14:19 +0700

    get Singer and get SingerById

--- src/app/service/singerManager/singer-manager.service.ts
+++ src/app/service/singerManager/singer-manager.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {AuthService} from '../userManager/auth/auth.service';
+import {Observable} from 'rxjs';
+import {SingerInfo} from '../../model/singer/singer-info';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SingerManagerService {
+  private readonly API_CREATE_SINGER = environment.URL + '/api/singers/create';
+  private readonly API_GET_SINGER = environment.URL + '/api/singers';
+  private readonly API_GET_SINGER_ID = environment.URL + '/api/singers';
+  private readonly API_PUT_SINGER = environment.URL + '/api/singers/update';
+  private readonly API_DELETE_SINGER = environment.URL + '/api/singers/delete';
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private authenService: AuthService) {
+  }
+
+  getSinger(): Observable<any> {
+    return this.http.get<any>(this.API_GET_SINGER);
+  }
+
+  getSingerById(id: number): Observable<SingerInfo> {
+    return this.http.get<SingerInfo>(`${this.API_GET_SINGER_ID}/${id}`);
+  }
+
 }
